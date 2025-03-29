6dba1041c6e728ea7c6d504decca7c1add0990c9
marcelwow <marcelek.waw@gmail.com>
2025-03-30 00:56:03 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("mongo dziala"))
+    .catch((err) => console.error("mongfo nie dziala:", err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+app.listen(5000, () => console.log("dziala na porcie:0000"));
