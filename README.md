9e91aa2bf2345567836308388b9205f46048995b
SewerynMajewski <seweryn.majewski@gmail.com>
2025-05-06 00:32:22 +0200

    Install bootstrap and add some basic design to pages

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Booking_58426_58662_53365_51264
 Aplikacja do rezerwacji pokoi hotelowych.
+
+
+### DevNotes:
+#### Bootstrap:
+This project utilizes **Bootstrap** for easier front end development.
+To develop this project you need to:
+`npm install bootstrap@5.3.5`
+
+Because **Bootstrap** is using .scss file format for it's Sass styling, it is also advised to use File Watchers.
+To do this you need to(steps for WebStorm IDE, in other it should be similar):
+1. Make sure you have Sass plugin installed in Webstorm
+2. `npm install -g sass`
+3. `ctrl + alt + s` and look for `File Watchers`
+4. Create a new file watcher for .scss
+
---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const config = require('./config');
+
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/../views'));
+
+// Homepage route
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Booking.com Clone',
+        message: 'Welcome to our Hotel Booking Website'
+    });
+});
+
+app.get('/hotel-view', (req, res) => {
+    res.render('hotel', {
+        title: 'Detailed hotel view',
+        message: 'This is a detailed hotel view'
+    })
+})
+
+// Start server
+app.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT}`);
+});
+
+
